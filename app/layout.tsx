08bb1608b135f2b67694cc858fdf5abc8b3a1eec
Melvin <melvin.r@iqvia.com>
2023-11-29 21:20:13 +0530

    Changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PageForm (@CodeWithKliton)",
-  description: "Check my youtube channel for more content",
+  title: "Mini MDM",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
